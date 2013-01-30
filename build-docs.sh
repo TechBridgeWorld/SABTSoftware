cb9b082682a8f971a83130a8b8f33edd4d3b0c4f
Maxime Bury <maxime.bury@gmail.com>
2013-01-30 15:54:12 -0500

    Improve the documentation building script

--- build-docs.sh
+++ build-docs.sh
@@ -1,5 +1,20 @@
 #!/bin/sh
+
+# Check that the documentation folder exists, if not create and initialize
+if [ ! -d documentation ]; then
+    mkdir documentation && cd documentation
+    
+    git clone git@github.com:CMU-15-239/SABTSoftware.git .
+    git checkout gh-pages
+    git branch -d master
+    
+    cd ..
+fi
+
+# Build docs
 doxygen Doxyfile
+
+# Push to gh-pages
 cd documentation
 git add .
 git commit -m "Updated documentation"
