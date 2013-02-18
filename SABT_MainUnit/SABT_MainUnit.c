144272ef400c1690d2ac63fac8d1fdc2a9870a07
Gary Giger <garyfredgiger@gmail.com>
2013-02-18 14:03:06 -0500

    fixed a bug

--- SABT_MainUnit/SABT_MainUnit.c
+++ SABT_MainUnit/SABT_MainUnit.c
@@ -167,7 +167,7 @@ ISR(USART1_RX_vect)
   USART_Keypad_Received_Data=UDR1;
   USART_Keypad_DATA_RDY=true; 
   USART_transmitByteToPC(USART_Keypad_Received_Data);
-  set_Last_Dot(USART_Keypad_Received_Data);
+  set_last_dot(USART_Keypad_Received_Data);
 };
 
 /**
