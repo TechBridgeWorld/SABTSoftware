1a8d4d3a760bef9878312bbad78321643c6752c5
Gary Giger <garyfredgiger@gmail.com>
2013-02-23 14:28:13 -0500

    Changes to mode 3

--- SABT_MainUnit/MD3.h
+++ SABT_MainUnit/MD3.h
@@ -9,6 +9,24 @@
 #ifndef _MD3_H_
 #define _MD3_H_
 
+// State definitions
+#define STATE_INITIAL        0   // Just started, plays welcome message
+#define STATE_REQUEST_INPUT1 1   // Request for input from user - part 1
+#define STATE_REQUEST_INPUT2 2   // Request for input from user - part 2
+#define STATE_WAIT_INPUT     3   // Waiting for user input
+#define STATE_PROC_INPUT     4   // Processed user input
+
+#define PRIME                53
+
+int current_state;               // current state of the mode
+char expected_dot;               // The expected response from user 
+char last_dot;                   // char representing last big dot pressed
+int animals_used_list[11];       // Int array representing the animals that 
+								 // have been used already.
+int animals_used;                // Keeps track of number of animals used in current rotation
+char* animal;                    // Name of current animal
+char current_letter;             // Current letter being checked for 
+
 void MD3_Main(void);
 void MD3_Reset(void);
 void MD3_CallModeYesAnswer(void);
