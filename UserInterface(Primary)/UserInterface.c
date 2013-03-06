f8b4c44798c05a55ec597a7ad5f2ea04f0555ea3
Nick LaGrow <nlagrow@andrew.cmu.edu>
2013-03-06 13:09:51 -0500

    Refactored UserInterface

--- UserInterface(Primary)/UserInterface.c
+++ UserInterface(Primary)/UserInterface.c
@@ -12,7 +12,11 @@
 
 void InitializeUI(void);
 
-void delay10()
+/**
+ * @brief debugging function to add delay
+ * @return Void
+ */
+void delay10(void)
 {
   int i = 0;
   int j=0;
@@ -21,150 +25,50 @@ void delay10()
       DDRD &= ~_BV(UI_VOLDOWN);
     }
   }
-  //PORTD &= ~_BV(UI_STAT1);
   return;
 }
 
-//uint8_t mcusr_mirror __attribute__ ((section (".noinit")));
-
-//reset
-/*ISR(_VECTOR(1)){
-  set_stat_led2(true);
-}*/
-
-/* TEST VECTORS
-ISR(_VECTOR(2)){
-  set_stat_led2(true);
-}
-ISR(_VECTOR(3)){
-  set_stat_led2(true);
-}
-ISR(_VECTOR(4)){
-  set_stat_led2(true);
-}
-ISR(_VECTOR(5)){
-  set_stat_led2(true);
-}
-ISR(_VECTOR(6)){
-  set_stat_led2(true);
-}
-ISR(_VECTOR(7)){
-  set_stat_led2(true);
-}
-ISR(_VECTOR(8)){
-  set_stat_led2(true);
-}
-ISR(_VECTOR(9)){
-  set_stat_led2(true);
-}
-ISR(_VECTOR(10)){
-  set_stat_led2(true);
-}*/
-/*ISR(_VECTOR(11)){
-  if(!LED_STAT){
-    set_stat_led2(true);
-    set_stat_led1(false);
-    LED_STAT=true;
-  }else{
-    set_stat_led2(false);
-    set_stat_led1(true);
-    LED_STAT=false;
-  }
-}*/
-/*ISR(_VECTOR(12)){
-  set_stat_led2(true);
-}
-ISR(_VECTOR(13)){
-  set_stat_led2(true);
-}
-ISR(_VECTOR(14)){
-  set_stat_led2(true);
-}
-ISR(_VECTOR(15)){
-  set_stat_led2(true);
-}
-ISR(_VECTOR(16)){
-  set_stat_led2(true);
-}
-ISR(_VECTOR(17)){
-  set_stat_led2(true);
-}
-ISR(_VECTOR(18)){
-  set_stat_led2(true);
-}
-ISR(_VECTOR(19)){
-  set_stat_led2(true);
-}*/
 ISR(_VECTOR(20)){
   transmit_complete = true;
   set_stat_led2(true);
-  //set_stat_led2(true);
-}/*
-ISR(_VECTOR(21)){
-  set_stat_led2(true);
 }
-ISR(_VECTOR(22)){
-  set_stat_led2(true);
-}
-ISR(_VECTOR(23)){
-  set_stat_led2(true);
-}
-ISR(_VECTOR(24)){
-  set_stat_led2(true);
-}
-ISR(_VECTOR(25)){
-  set_stat_led2(true);
-}
-ISR(_VECTOR(26)){
-  set_stat_led2(true);
-}
-*/
 
 /**
  * @brief the main execution loop for the primary ui board
  * Executes an infinite loop, should never return
- * @return 0
+ * @return Void
  */
-int main(void)//
+void main(void)//
 {
-  InitializeUI();
+  // Set up interfaces 
+  initialize_ui();
 
-  while(1) // Main loop
-  // This loop will take care of all the signal handling from the 
+  // Infinite processing loop
+  while(1)
   {
-    // Test the transmission to MCU 
-    //USART_transmitStringToMCU((unsigned char*)"A message from the Primary UI Board.\r\n");
-
     // Check to see if timer interrupt has occurred
-    if(timer_interrupt)
-    {
-      TimerRoutine();
-    }
+    if(timer_interrupt) timer_routine();
   }
-
-  return 0;
 }
 
 /**
  * @brief initialize basic elements of the UI board
  * @return Void
  */
-void InitializeUI(void)
+void initialize_ui(void)
 {
-  //Initialize the Serial comm with the main module
-  init_USART_MCU();
+  // Initialize the serial communication with the main module
+  init_usart_mcu();
 
-  //Initialize the analog ports for intermediate/advanced boards only (double check?)
-  //InitializeAnalogPorts();
-
-  //Initialize the digital input/output pins
-  Initialize_Digital_IO();
+  // Initialize the digital input/output pins
+  initialize_digital_io();
 
   // Initialize the timer
-  initTimer();
+  init_timer();
 
   // Enable interrupts
   sei();
 
-  InterfaceType=1;
+  // Start in writing mode
+  interface_type = INTERFACE_WRITING;
 }
